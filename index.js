02760188d5d9b0e1304eca5f96d7df027ca0c2e5
Rakesh Patel <emailme@rakeshrpatel.com>
2016-09-19 07:47:47 -0400

    new updates

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+var express = require('express'),
+    httpRequest = require('request'),
+    app = express(),
+    http = rquire('http'),
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 8000,
+    FACEBOOK_PAGE_ACCESS_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
+
 app.get('/webhook/', function(req, res) {
     if (req.query['hub.verify_token'] === 'super_secret_token') {
         res.send(req.query['hub.challenge']);
     }
     res.send('Error, wrong validation token');
 })
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
